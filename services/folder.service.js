59d744a9ce47c692d64207f430969b2b3b07d440
Arber Shabani <arber.shabani@hotmail.com>
2020-04-12 23:42:09 +0200

    Fix Copying Assets to the specified folders

--- services/folder.service.js
+++ services/folder.service.js
@@ -1,4 +1,21 @@
 const fs = require("fs");
+const path = require("path");
+
+var copyRecursiveSync = function (src, dest) {
+	var exists = fs.existsSync(src);
+	var stats = exists && fs.statSync(src);
+	var isDirectory = exists && stats.isDirectory();
+	if (isDirectory) {
+		if (!fs.existsSync(dest)) fs.mkdirSync(dest);
+		fs.readdirSync(src).forEach(function (childItemName) {
+			copyRecursiveSync(path.join(src, childItemName), path.join(dest, childItemName));
+		});
+	} else {
+		fs.copyFile(src, dest, (err) => {
+			if (err) console.log(err);
+		});
+	}
+};
 
 module.exports = {
 	reports: (name) => {
@@ -9,7 +26,10 @@ module.exports = {
 			let defaultFolder = "./cypress/results";
 			const folderNames = name.split("/");
 			folderNames.map((folder, i) => {
-				if (folderNames.length - 1 !== i) {
+				if (folderNames.length - 1 === i) {
+					if (!fs.existsSync(defaultFolder + "/assets")) fs.mkdirSync(defaultFolder + "/assets");
+					copyRecursiveSync("./cypress/results/assets", defaultFolder + "/assets");
+				} else {
 					defaultFolder = defaultFolder + "/" + folder;
 					if (!fs.existsSync(defaultFolder)) fs.mkdirSync(defaultFolder);
 				}
