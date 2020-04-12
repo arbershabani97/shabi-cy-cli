e610356d3901e0a5104a3c60ea60e86e1187e51d
Arber Shabani <arber.shabani@hotmail.com>
2020-04-13 00:03:06 +0200

    Remove Assets if there is no file in the folder

--- conversion/reports.js
+++ conversion/reports.js
@@ -3,7 +3,7 @@ const checkFolder = require("../services/folder.service");
 module.exports = () => {
 	fs.readdir("cypress/results", function (err, files) {
 		if (err) return console.log("Unable to scan directory: " + err);
-		files.forEach((file) => {
+		files.forEach((file, i) => {
 			if (file.endsWith(".json")) {
 				const obj = JSON.parse(fs.readFileSync(`cypress/results/${file}`, "utf8"));
 				const fileName = obj.results[0].file.split("\\").slice(2).join("/").split(".json")[0] + ".html";
@@ -12,6 +12,17 @@ module.exports = () => {
 				fs.rename(`cypress/results/${file.split(".json")[0] + ".html"}`, `cypress/results/${fileName}`, function (err) {
 					if (err) throw err;
 					console.log("File Renamed.");
+					fs.unlink(`cypress/results/${file}`, function (err) {
+						if (err) console.log(err);
+						if (i === files.length - 1) {
+							fs.readdir("cypress/results", function (err, files) {
+								if (err) console.log(err);
+								if (!files.find((a) => a.includes("."))) {
+									checkFolder.removeAssets("./cypress/results/assets");
+								}
+							});
+						}
+					});
 				});
 			}
 		});
--- services/folder.service.js
+++ services/folder.service.js
@@ -1,6 +1,20 @@
 const fs = require("fs");
 const path = require("path");
 
+function rimraf(dir_path) {
+	if (fs.existsSync(dir_path)) {
+		fs.readdirSync(dir_path).forEach(function (entry) {
+			var entry_path = path.join(dir_path, entry);
+			if (fs.lstatSync(entry_path).isDirectory()) {
+				rimraf(entry_path);
+			} else {
+				fs.unlinkSync(entry_path);
+			}
+		});
+		fs.rmdirSync(dir_path);
+	}
+}
+
 var copyRecursiveSync = function (src, dest) {
 	var exists = fs.existsSync(src);
 	var stats = exists && fs.statSync(src);
@@ -35,5 +49,8 @@ module.exports = {
 				}
 			});
 		}
+	},
+	removeAssets: (name) => {
+		rimraf(name);
 	},
 };
