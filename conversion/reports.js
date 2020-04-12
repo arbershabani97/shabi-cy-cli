ba3ad18cdf36efc7973cbc0795c194786f81b031
Arber Shabani <arber.shabani@hotmail.com>
2020-04-13 00:21:10 +0200

    Changing to Promises

--- conversion/reports.js
+++ conversion/reports.js
@@ -1,30 +1,23 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const checkFolder = require("../services/folder.service");
-module.exports = () => {
-	fs.readdir("cypress/results", function (err, files) {
-		if (err) return console.log("Unable to scan directory: " + err);
-		files.forEach((file, i) => {
+module.exports = async () => {
+	const files = await fs.readdir("cypress/results");
+	await Promise.all(
+		files.map(async (file, i) => {
 			if (file.endsWith(".json")) {
-				const obj = JSON.parse(fs.readFileSync(`cypress/results/${file}`, "utf8"));
+				const obj = JSON.parse(await fs.readFile(`cypress/results/${file}`, "utf8"));
 				const fileName = obj.results[0].file.split("\\").slice(2).join("/").split(".json")[0] + ".html";
 
 				checkFolder.reports(fileName);
-				fs.rename(`cypress/results/${file.split(".json")[0] + ".html"}`, `cypress/results/${fileName}`, function (err) {
-					if (err) throw err;
-					console.log("File Renamed.");
-					fs.unlink(`cypress/results/${file}`, function (err) {
-						if (err) console.log(err);
-						if (i === files.length - 1) {
-							fs.readdir("cypress/results", function (err, files) {
-								if (err) console.log(err);
-								if (!files.find((a) => a.includes("."))) {
-									checkFolder.removeAssets("./cypress/results/assets");
-								}
-							});
-						}
-					});
-				});
+				await fs.rename(`cypress/results/${file.split(".json")[0] + ".html"}`, `cypress/results/${fileName}`);
+				console.log("File Renamed.");
+				await fs.unlink(`cypress/results/${file}`);
 			}
-		});
-	});
+		}),
+	);
+
+	const _files = await fs.readdir("cypress/results");
+	if (!_files.find((a) => a.includes("."))) {
+		checkFolder.removeAssets("./cypress/results/assets");
+	}
 };
