f9f5737bc8163d4ac1614fc007c0ba13a2ac9eb0
Arber Shabani <arber.shabani@hotmail.com>
2020-04-14 21:01:16 +0200

    Delete Tests Improve Logic

--- components/deleteTests.js
+++ components/deleteTests.js
@@ -1,28 +1,22 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 
-module.exports = (tests) => {
-    let existingTests = []
-	fs.readdir("cypress/integration", function (err, files) {
-        if (err) return console.log("Unable to scan directory: " + err);
+module.exports = async (tests) => {
+	let existingTests = [];
 
-        tests.forEach((_test) => {
-            const existingFile = files.find((file) => Number(file.slice(2, 5)) === Number(_test));
-            if (existingFile) existingTests.push(existingFile);
-        });
+	const files = await fs.readdir("cypress/integration");
 
-        existingTests.forEach((existingFile,i)=>{
-            fs.unlink(`cypress/integration/${existingFile}`, function (err) {
-                if (err) {
-                    console.log(`${existingFile} failed to delete!`);
-                } else {
-                    console.log(`${existingFile} deleted!`);
-                }
-                if(i === existingTests.length - 1) process.exit();
-            });
-        });
-        if(!existingTests.length) {
-            console.log("No tests found!");
-            process.exit();
-        }
+	tests.forEach((_test) => {
+		const existingFile = files.find((file) => Number(file.slice(2, 5)) === Number(_test));
+		if (existingFile) existingTests.push(existingFile);
 	});
+
+	await Promise.all(
+		existingTests.map(async (existingFile, i) => {
+			await fs.unlink(`cypress/integration/${existingFile}`);
+			console.log(`${existingFile} deleted!`);
+		}),
+	);
+	if (!existingTests.length) console.log("No tests found!");
+
+	process.exit();
 };
