994dc1e232ce7cfe684278182afd09e4bdc8251a
Arber Shabani <arber.shabani@hotmail.com>
2020-04-14 19:37:09 +0200

    Run Tests in Subfolders

--- components/runTest.js
+++ components/runTest.js
@@ -1,8 +1,12 @@
 const fs = require("fs");
-const {spawn} = require("child_process");
+const {exec} = require("child_process");
 
 module.exports = (name) => {
-	fs.readdir("cypress/integration", function (err, files) {
+	let folder = name.split("/").slice(0, -1).join("/");
+	if (folder) folder = `/${folder}/`;
+	name = name.split("/").pop();
+
+	fs.readdir(`cypress/integration${folder}`, function (err, files) {
 		if (err) return console.log("Unable to scan directory: " + err);
 		const existingFile = files.find((file) => Number(file.slice(2, 5)) === Number(name));
 		let spawnedProcess = spawn("npm", ["run", "file", `cypress/**/${existingFile}`], {stdio: "inherit", shell: true});
